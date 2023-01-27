c6a5de460dbf9ef380477b83be8dbc859a308363
Swapnil Landage <97462546+Swapnil1296@users.noreply.github.com>
2023-01-27 15:50:18 +0530

    Initial commit

---
+++ src/Menu.js
@@ -0,0 +1,11 @@
+export default function Menu() {
+  return (
+    <>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <a href>Item 1</a>
+        <a href>Item 2</a>
+        <a href>Item 3</a>
+      </div>
+    </>
+  );
+}
